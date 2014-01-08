4cf54e47357249e46a5ffb91420ed60762a82b9e
Erik Cunningham <djtrinary@gmail.com>
2014-01-07 18:06:26 -0700

    slowly going insane

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function (grunt) {
                 },
                 files: [
                     '<%= yeoman.app %>/*.html',
-                    '<%= yeoman.app %>/elements/**/*.html',
+                    '<%= yeoman.app %>/elements/*.html',
                     '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
                     '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
